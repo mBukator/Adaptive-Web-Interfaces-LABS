e620dd67109f4dd771bb48493cc8fd475b2e6a59
mBukator <m.bukator03@gmail.com>
2024-02-25 02:17:12 +0200

    LAB 4

---
+++ LR4/LR4/Program.cs
@@ -0,0 +1,43 @@
+﻿using LR4.Services;
+using System;
+using System.Reflection;
+
+class Program {
+    static void Main(String[] args) {
+        Games valheim = new Games(1, "Valheim", 'A', 98.3, true);
+        Games stickman = new Games(2, "Stickman", 'C', 2.6, false);
+
+        valheim.GetFullInfo();
+        stickman.GetGameId(stickman);
+
+
+        // Using Type and TypeInfo
+        Type valheimType = valheim.GetType();
+        TypeInfo valheimTypeInfo = valheimType.GetTypeInfo();
+
+        Console.WriteLine("\n====[ Type & TypeInfo ]====\n"
+                          + "Valheim Type (BaseType): " + valheimType.BaseType
+                          + "\nValheim TypeInfo (Namespace): " + valheimTypeInfo.Namespace) ;
+
+
+        // Using MemberInfo
+        Console.WriteLine("\n====[ MemberInfo ]====");
+        foreach (MemberInfo member in valheimTypeInfo.GetMembers()) {
+            Console.WriteLine(member.DeclaringType + " - " + member.Name + " - " + member.MemberType);
+        }
+
+
+        // Using FieldInfo
+        Console.WriteLine("\n====[ FieldInfo ]====");
+        foreach (FieldInfo fieldInfo in valheimTypeInfo.DeclaredFields) {
+            Console.WriteLine(fieldInfo.Name + " - " + fieldInfo.FieldType);
+        }
+
+
+        // Using MethodInfo
+        Console.WriteLine("\n====[ MethodInfo ]====");
+        foreach (MethodInfo methodInfo in valheimTypeInfo.GetMethods()) {
+            Console.WriteLine(methodInfo.Name + " - " + methodInfo.ReturnType + " - " + methodInfo.GetType());
+        }
+    }
+}
---
+++ LR4/LR4/Services/Games.cs
@@ -0,0 +1,38 @@
+﻿namespace LR4.Services {
+    internal class Games {
+        private int _id;
+        public string name;
+        internal char gameClassification;
+        private protected double percentOfPositiveReviews;
+        protected bool isTopSellerOnSteam;
+
+
+        //  Class constructor
+        public Games(int _id, string name, char gameClassification, double percentOfPositiveReviews, bool isTopSellerOnSteam) { 
+            this._id = _id;
+            this.name = name;
+            this.gameClassification = gameClassification;
+            this.percentOfPositiveReviews = percentOfPositiveReviews;
+            this.isTopSellerOnSteam = isTopSellerOnSteam;
+        }
+
+
+        // Class methods
+        public void GetFullInfo() {
+            string sellingInfo = isTopSellerOnSteam ? "Top seller" : "Not top seller";
+            Console.WriteLine("====[ " + name + " ]====\n"
+                              + "[+] Positive reviews: " + percentOfPositiveReviews + "%\n"
+                              + "[+] Game Classification: " + gameClassification + "\n"
+                              + "[+] Game is: " + sellingInfo + "\n");
+        }
+
+        internal protected void GetGameId(Games game) {
+            Console.WriteLine("ID of " + game.name + " is: " + game._id);
+        }
+
+        protected void GetPercentOfPositiveReviews(Games game) {
+            Console.WriteLine("Positive reviews: " + game.percentOfPositiveReviews + "%");
+        }
+
+    }
+}
